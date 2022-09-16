d1391ac585184fca17b29ffd5f3859f9dcb60016
simon tang <simont.codes@gmail.com>
2022-09-16 13:49:27 -0400

    modal creates new shifts

--- src/components/CalendarDay/CalendarDay.js
+++ src/components/CalendarDay/CalendarDay.js
@@ -2,23 +2,23 @@ import CalendarDayBlock from "../CalendarDayBlock/CalendarDayBlock";
 import "./CalendarDay.scss";
 
 const CalendarDay = ({ day, employeeList, activeEmployee }) => {
-
-    return (
-        <>
-            <div className="day">
-                <div className="day-title block block--title">{day}</div>
-                {employeeList.map((employee) => {
-                    return (
-                        <CalendarDayBlock
-                            activeEmployee={activeEmployee}
-                            employee={employee}
-                            day={day}
-                        />
-                    );
-                })}
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="day">
+        <div className="day-title block block--title">{day}</div>
+        {employeeList.map((employee) => {
+          return (
+            <CalendarDayBlock
+              employeeList={employeeList}
+              activeEmployee={activeEmployee}
+              employee={employee}
+              day={day}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
 };
 
 export default CalendarDay;
---
+++ src/components/TicketShift/TicketShift.js
@@ -0,0 +1,12 @@
+
+
+
+function TicketShift() {
+  return (
+    <div className="day-block__ticket">
+      <div className="day-block__type"></div>
+    </div>
+  );
+}
+
+export default TicketShift;
