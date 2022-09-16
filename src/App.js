35ba145d511ee356501e785dfbdfe288dea4e17b
Moin Sekander <moinuddin8510@gmail.com>
2022-09-16 09:44:30 -0400

    clean up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.scss';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<h1>Hello World!</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
